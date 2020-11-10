7cb9046aa799ff0b50ea8d86bfc2a86e74c98a28
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2020-11-11 06:59:33 +0900

    Load and launch SimpleCov at the very top of test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,14 +3,14 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-require "minitest"
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
 if ENV["CI"]
   require "simplecov"
   SimpleCov.start "rails" do
     add_filter "/test/"
   end
 end
+
+# Filter out Minitest backtrace while allowing backtrace from other libraries
+# to be shown.
+require "minitest"
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
